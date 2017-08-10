2606e0a4768717c545af749d1d5d3d982bc91953
Rafał Chodzidło <raflotraflo@gmail.com>
2017-08-10 17:35:02 +0200

    Web API

---
+++ Angular2MVC/Controllers/UserAPIController.cs
@@ -0,0 +1,36 @@
+﻿using Angular2MVC.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Angular2MVC.Controllers
+{
+    public class UserAPIController : BaseAPIController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(UserDB.TblUser.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody]TblUser value)
+        {
+            UserDB.TblUser.Add(value);
+            return ToJson(UserDB.SaveChanges());
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]TblUser value)
+        {
+            UserDB.Entry(value).State = EntityState.Modified;
+            return ToJson(UserDB.SaveChanges());
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            UserDB.TblUser.Remove(UserDB.TblUser.FirstOrDefault(x => x.Id == id));
+            return ToJson(UserDB.SaveChanges());
+        }
+    }
+}
