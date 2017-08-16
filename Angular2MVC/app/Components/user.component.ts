2f91815d6c944bbd992a1d31232fe10e2ed7795c
Rafał Chodzidło <raflotraflo@gmail.com>
2017-08-16 22:30:25 +0200

    part2 Bartkowka

---
+++ Angular2MVC/app/Components/user.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit, ViewChild } from '@angular/core';
+import { UserService } from '../Service/user.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+import { IUser } from '../Models/user';
+import { DBOperation } from '../Shared/enum';
+import { Observable } from 'rxjs/Rx';
+import { Global } from '../Shared/global';
+
+@Component({
+
+    templateUrl: 'app/Components/user.component.html'
+
+})
+
+export class UserComponent implements OnInit {
+
+        ngOnInit(): void {
+            throw new Error('Method not implemented.');
+        }
+
+    @ViewChild('modal') modal: ModalComponent;
+    users: IUser[];
+    user: IUser;
+    msg: string;
+    indLoading: boolean = false;
+    userFrm: FormGroup;
+    dbops: DBOperation;
+    modalTitle: string;
+    modalBtnTitle: string;
+}
--- Angular2MVC/app/app.module.ts
+++ Angular2MVC/app/app.module.ts
@@ -6,12 +6,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 import { HomeComponent } from './components/home.component';
+import { UserService } from './Service/user.service'
 
  
 @NgModule({
     imports: [BrowserModule, ReactiveFormsModule, HttpModule, routing],
     declarations: [AppComponent, HomeComponent],
-    providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
+    providers: [{ provide: APP_BASE_HREF, useValue: '/' }, UserService],
     bootstrap: [AppComponent]
 })
 
